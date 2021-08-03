4b2975602c0ca42e64e85abbcd4e9741d019d421
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-08-03 11:47:57 -0300

    criou arquivo quero ser um ninja

---
+++ src/components/QueroSerUmNinja.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class QueroSerUmNinja extends React.Component{
+    render(){
+        return <div>
+            <p>QueroSerUmNinja</p>
+        </div>
+    }
+}
