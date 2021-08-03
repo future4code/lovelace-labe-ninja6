832e77d0c27161c85d0f946b1abeb8e247f1ca66
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-08-03 14:29:32 -0300

    header AppContainer

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import Styled from 'styled-components'
+
 import CardServico from './CardServico'
 import Carrinho from './Carrinho'
 import Contratar from './Contratar'
 import Detalhes from './Detalhes'
 import Home from './Home'
 import QueroSerUmNinja from './QueroSerUmNinja'
+
+const StyledAppContainer = Styled.div`
+  width: 100%;
+`
+const StyledHeader = Styled.header`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  background: gray;
+`
+const StyledMain = Styled.main`
+  width: 100%;
+`
 
 export class AppContainer extends Component {
 
@@ -39,21 +54,25 @@ export class AppContainer extends Component {
 
   render() {
     return (
-      <div>
-        <p>Pronto para começar!</p>
-        <div>
-          <button onClick={() => { this.setState({ paginaAtual: 'home' }) }}>Home</button>
-          <button onClick={() => { this.setState({ paginaAtual: 'queroserumninja' }) }}>Quero ser um ninja</button>
-          <button onClick={() => { this.setState({ paginaAtual: 'detalhes' }) }}>Detalhes</button>
-          <button onClick={() => { this.setState({ paginaAtual: 'carrinho' }) }}>Carrinho</button>
-          <button onClick={() => { this.setState({ paginaAtual: 'contratar' }) }}>Contratar</button>
-          <button onClick={() => { this.setState({ paginaAtual: 'cardGustavoServico' }) }}>cardGustavoServico</button>
+      <StyledAppContainer>
+        <StyledHeader>          
+          <p>Esse header é só um teste!!!</p>
+          <div>
+            <button onClick={() => { this.setState({ paginaAtual: 'home' }) }}>Home</button>
+            <button onClick={() => { this.setState({ paginaAtual: 'queroserumninja' }) }}>Quero ser um ninja</button>
+            <button onClick={() => { this.setState({ paginaAtual: 'detalhes' }) }}>Detalhes</button>
+            <button onClick={() => { this.setState({ paginaAtual: 'carrinho' }) }}>Carrinho</button>
+            <button onClick={() => { this.setState({ paginaAtual: 'contratar' }) }}>Contratar</button>
+            <button onClick={() => { this.setState({ paginaAtual: 'cardGustavoServico' }) }}>cardGustavoServico</button>
 
-        </div>
-        {
-          this.paginaSelecionada()
-        }
-      </div>
+          </div>
+        </StyledHeader>
+        <StyledMain>
+          {
+            this.paginaSelecionada()
+          }
+        </StyledMain>
+      </StyledAppContainer>
     )
   }
 }
