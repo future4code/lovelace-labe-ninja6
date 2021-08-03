87eca045447888c76e6c2c2f8888872d82e66ab7
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-08-03 11:57:54 -0300

    swith para mudar pagina

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
 import Home from './Home'
+import QueroSerUmNinja from './QueroSerUmNinja'
 
 export class AppContainer extends Component {
+
+  state = {
+    paginaAtual: 'home'
+  }
+
+  paginaSelecionada = () => {
+    switch (this.state.paginaAtual) {
+      case 'home':
+        return <Home />
+    
+      case 'queroserumninja':
+        return <QueroSerUmNinja />
+
+      default:
+        return <p>Erro</p>
+    }
+  }
+
   render() {
     return (
       <div>
         <p>Pronto para começar!</p>
-        <Home />
+        <div>
+          <button onClick={() => { this.setState({ paginaAtual: 'home' }) }}>Home</button>
+          <button onClick={() => { this.setState({ paginaAtual: 'queroserumninja' }) }}>Quero ser um ninja</button>
+        </div>
+        {
+          this.paginaSelecionada()
+        }
       </div>
     )
   }
