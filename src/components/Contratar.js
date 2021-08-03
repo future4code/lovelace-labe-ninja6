d58655c48ca0b0f2f606f87ed5ab8c5e0b1b6f9d
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-08-03 12:13:53 -0300

    arquivos criados

---
+++ src/components/Contratar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Contratar extends React.Component{
+    render(){
+        return <div>
+            <p>Contratar</p>
+        </div>
+    }
+}
---
+++ src/components/Detalhes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Detalhes extends React.Component{
+    render(){
+        return <div>
+            <p>Detalhes</p>
+        </div>
+    }
+}
