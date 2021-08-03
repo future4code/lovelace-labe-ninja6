dc88862c0c6c7810dcfd19b37bfc60f7f07a44e3
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-08-03 15:52:05 -0300

    Componente Carrinho Finalizado

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import CardServico from './CardServico'
-import Carrinho from './Carrinho'
+import Carrinho from './Carrinho/Carrinho'
 import Contratar from './Contratar'
 import Detalhes from './Detalhes'
 import Home from './Home'
@@ -8,52 +8,70 @@ import QueroSerUmNinja from './QueroSerUmNinja'
 
 export class AppContainer extends Component {
 
-  state = {
-    paginaAtual: 'home'
-  }
-
-  paginaSelecionada = () => {
-    switch (this.state.paginaAtual) {
-      case 'home':
-        return <Home />
-    
-      case 'queroserumninja':
-        return <QueroSerUmNinja />
-
-      case 'detalhes':
-        return <Detalhes />
-
-      case 'carrinho':
-        return <Carrinho />
-
-      case 'contratar':
-        return <Contratar />
-
-      case 'cardGustavoServico':
-        return <CardServico />
-  
-      default:
-        return <p>Erro</p>
+    state = {
+        paginaAtual: 'home'
     }
-  }
 
-  render() {
-    return (
-      <div>
-        <p>Pronto para começar!</p>
-        <div>
-          <button onClick={() => { this.setState({ paginaAtual: 'home' }) }}>Home</button>
-          <button onClick={() => { this.setState({ paginaAtual: 'queroserumninja' }) }}>Quero ser um ninja</button>
-          <button onClick={() => { this.setState({ paginaAtual: 'detalhes' }) }}>Detalhes</button>
-          <button onClick={() => { this.setState({ paginaAtual: 'carrinho' }) }}>Carrinho</button>
-          <button onClick={() => { this.setState({ paginaAtual: 'contratar' }) }}>Contratar</button>
-          <button onClick={() => { this.setState({ paginaAtual: 'cardGustavoServico' }) }}>cardGustavoServico</button>
+    paginaSelecionada = () => {
+        switch (this.state.paginaAtual) {
+            case 'home':
+                return <Home/>
 
-        </div>
-        {
-          this.paginaSelecionada()
+            case 'queroserumninja':
+                return <QueroSerUmNinja/>
+
+            case 'detalhes':
+                return <Detalhes/>
+
+            case 'carrinho':
+                return <Carrinho/>
+
+            case 'contratar':
+                return <Contratar/>
+
+            case 'cardGustavoServico':
+                return <CardServico/>
+
+            default:
+                return <p>Erro</p>
         }
-      </div>
-    )
-  }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Pronto para começar!</p>
+                <div>
+                    <button onClick={() => {
+                        this.setState({paginaAtual: 'home'})
+                    }}>Home
+                    </button>
+                    <button onClick={() => {
+                        this.setState({paginaAtual: 'queroserumninja'})
+                    }}>Quero ser um ninja
+                    </button>
+                    <button onClick={() => {
+                        this.setState({paginaAtual: 'detalhes'})
+                    }}>Detalhes
+                    </button>
+                    <button onClick={() => {
+                        this.setState({paginaAtual: 'carrinho'})
+                    }}>Carrinho
+                    </button>
+                    <button onClick={() => {
+                        this.setState({paginaAtual: 'contratar'})
+                    }}>Contratar
+                    </button>
+                    <button onClick={() => {
+                        this.setState({paginaAtual: 'cardGustavoServico'})
+                    }}>cardGustavoServico
+                    </button>
+
+                </div>
+                {
+                    this.paginaSelecionada()
+                }
+            </div>
+        )
+    }
 }
---
+++ src/components/Carrinho/Carrinho.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import * as All from "./Carrinho.styles"
+import {Button} from "@material-ui/core";
+
+
+
+
+export default class Carrinho extends React.Component {
+    render() {
+        return (
+            <All.ContainerItem>
+                
+                <All.Items>
+                    <h3>Titulo do serviço</h3>
+
+                    <div>
+                        <p>R$ 400,00</p>
+                        <Button variant="contained">Remover</Button>
+                    </div>
+                </All.Items>
+
+                <All.Total>
+                    <h3>Valor Total: <span>R$ 400,00</span></h3>
+
+                    <Button variant="contained">Contratar Serviço</Button>
+                </All.Total>
+            </All.ContainerItem>
+        )
+    }
+}
