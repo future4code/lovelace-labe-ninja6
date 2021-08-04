b2f8a52cbb63a35ae7373d9a659679cf45b3c1cc
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2021-08-04 10:51:41 -0300

    Pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from 'react'
+import * as HomeStyles from "./App.styles"
 import {ThemeProvider} from "@material-ui/styles";
 import {theme} from "./theme";
 
 //Componentes
-import { AppContainer } from './components/AppContainer'
+// import { AppContainer } from './components/AppContainer'
+import CardServico from './components/CardServico'
+import Carrinho from './components/ComponentCarrinho/Carrinho'
+import Contratar from './components/Contratar'
+import Detalhes from './components/Detalhes'
+import Home from './components/ComponentHome/Home'
+import QueroSerUmNinja from './components/QueroSerUmNinja'
 
-function App() {
-	return (
-		<ThemeProvider theme={theme}>
-			<AppContainer />
+//Images
+import logoNinjas from "./img/logo-peq.jpg";
 
-		</ThemeProvider>
-	)
+class App extends React.Component{
+	state = {
+
+	}
+
+	render() {
+		return (
+			<ThemeProvider theme={theme}>
+				<HomeStyles.Header>
+
+					<HomeStyles.LogoPeq src={logoNinjas} alt="Logo da LabeNinjas"/>
+					<h2>LabeNinjas</h2>
+
+				</HomeStyles.Header>
+
+				<Home />
+
+
+
+				{/*<AppContainer />*/}
+
+			</ThemeProvider>
+		)
+	}
 }
 
 export default App
---
+++ src/App.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+    display: flex;
+    color: white;
+    align-items: center;
+    background-color: #464866;
+    height: 60px;
+`
+export const LogoPeq = styled.img`
+    height: 45px;
+    margin-left: 15px;
+    margin-right: 15px;
+`
