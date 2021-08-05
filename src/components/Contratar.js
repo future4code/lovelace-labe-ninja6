5e897039eced14f330637f2669b38477c04eb63d
Gustavo Guimaraes <Gustavo.carvg@gmail.com>
2021-08-05 16:26:31 -0300

    Trabalhada a responsividade na pagina contratar um ninja

--- src/components/Contratar.js
+++ src/components/Contratar.js
@@ -14,11 +14,24 @@ import {
 import axios from "axios";
 
 const StyledContainerContratar = Styled.div`
+
+
+    @media (min-width: 320px) {
+      display: flex;
+      flex-direction: column;
+      }
+    @media (min-width: 768px) {
+      display: flex;
+      flex-direction: column;
+      }
+    @media (min-width: 1200px) {
     width: 100%;
     min-height: 94.2vh;
     display: grid;
     grid-template-columns: 1fr 5fr;
     grid-template-rows: 1fr 9fr;
+
+      }
 `;
 
 const StyledContainerControles = Styled.div`
@@ -39,7 +52,31 @@ const StyledContainerControles = Styled.div`
 `;
 
 const StyledContainerBusca = Styled.div`
-    grid-column: 1 / 2;
+
+    @media (min-width: 320px ) {
+      display: flex;
+      flex-direction: column;
+      background: #AAABB8;
+      padding: 2%;
+      margin: 5px auto;
+      justify-content: center;
+      align-items: center;
+      max-height: 250px;
+      width: 90%;
+      max-width:500px;
+      
+      
+      }
+
+    @media (min-width: 768px) {
+    min-width: 500px;
+            margin: 5px auto;
+           
+      }
+    @media (min-width: 1200px) {
+
+
+      grid-column: 1 / 2;
     grid-row: 2 / 3;
     display: flex;
     background: #AAABB8;
@@ -48,6 +85,9 @@ const StyledContainerBusca = Styled.div`
     padding-left: 5%;
     margin: 10px;
     min-width: 120px;
+
+      }
+    
 `;
 
 const StyledInputBuscar = Styled(Input)`
@@ -107,15 +147,19 @@ const StyledCardServico = Styled.div`
         justify-content: space-around;
         margin: 10px;
     }
+    Button {
+      margin: 5px;
+      min-width: 130px;
+    }
 `;
 
 export default class Contratar extends React.Component {
   state = {
     listaDeServicos: [],
-    inputBuscar: '',
-    inputOrdenar: '',
+    inputBuscar: "",
+    inputOrdenar: "",
     inputValorMinimo: -Infinity,
-    inputValorMaximo: Infinity
+    inputValorMaximo: Infinity,
   };
 
   componentDidMount() {
@@ -141,68 +185,64 @@ export default class Contratar extends React.Component {
   };
 
   render() {
-    const listaFiltrada = this.state.listaDeServicos.filter( produto => {
-                            const aProcurar = produto.title.toLowerCase()
-                            return aProcurar.includes((this.state.inputBuscar).toLowerCase())  
-                        
-                        }).filter(produto => {
-                            return produto.price < this.state.inputValorMaximo
-                        
-                        }).filter(produto => {
-                            return produto.price > this.state.inputValorMinimo
-                        
-                        }).sort( (produtoA, produtoB) => {
-                            if(produtoA===produtoB)
-                                return 0;
+    const listaFiltrada = this.state.listaDeServicos
+      .filter((produto) => {
+        const aProcurar = produto.title.toLowerCase();
+        return aProcurar.includes(this.state.inputBuscar.toLowerCase());
+      })
+      .filter((produto) => {
+        return produto.price < this.state.inputValorMaximo;
+      })
+      .filter((produto) => {
+        return produto.price > this.state.inputValorMinimo;
+      })
+      .sort((produtoA, produtoB) => {
+        if (produtoA === produtoB) return 0;
 
-                            switch (this.state.inputOrdenar) {
-                                case 'titulo':
-                                    if((produtoA.title).toLowerCase() > (produtoB.title).toLowerCase())
-                                        return 1;
-                                    else
-                                        return -1;
-                            
-                                case 'precoCrescente':
-                                    if(produtoA.price > produtoB.price)
-                                        return 1;
-                                    else
-                                        return -1;
+        switch (this.state.inputOrdenar) {
+          case "titulo":
+            if (produtoA.title.toLowerCase() > produtoB.title.toLowerCase())
+              return 1;
+            else return -1;
 
-                                case 'precoDecrescente':
-                                    if(produtoA.price < produtoB.price)
-                                        return 1;
-                                    else
-                                        return -1;
+          case "precoCrescente":
+            if (produtoA.price > produtoB.price) return 1;
+            else return -1;
 
-                                case 'prazo':
-                                    if(Date.parse(produtoA.dueDate) > Date.parse(produtoB.dueDate))
-                                        return 1;
-                                    else
-                                        return -1;
+          case "precoDecrescente":
+            if (produtoA.price < produtoB.price) return 1;
+            else return -1;
 
-                                default:
-                                    break;
-                            }
-                        })
+          case "prazo":
+            if (Date.parse(produtoA.dueDate) > Date.parse(produtoB.dueDate))
+              return 1;
+            else return -1;
+
+          default:
+            break;
+        }
+      });
 
     const listaFinalServicos = listaFiltrada.map((servico) => {
-      const date = new Date(servico.dueDate)
+      const date = new Date(servico.dueDate);
       return (
         <StyledCardServico key={servico.id}>
           <h4>{servico.title}</h4>
           <p>
-
-            Prazo: {date.getDate()+ "/" + date.getMonth() + "/" + date.getFullYear()} por apenas <strong>{servico.price}</strong>
+            Prazo:{" "}
+            {date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear()}{" "}
+            por apenas <strong>{servico.price}</strong>
           </p>
           <div>
+            <Button onClick={this.props.VerDetalhes} variant="contained">
+              Ver detalhes
+            </Button>
             <Button
-                onClick={this.props.VerDetalhes}
-                variant="contained">Ver detalhes</Button>
-            <Button
-                onClick={this.props.AddCarrinho}
+              onClick={this.props.AddCarrinho}
               variant="contained"
               color="primary"
-              startIcon={<AddShoppingCartIcon />}>
+              startIcon={<AddShoppingCartIcon />}
+            >
               Adicionar
             </Button>
           </div>
@@ -223,51 +263,47 @@ export default class Contratar extends React.Component {
         </StyledContainerControles>
 
         <StyledContainerBusca>
-          <StyledInputBuscar 
-            placeholder="Buscar" 
+          <StyledInputBuscar
+            placeholder="Buscar"
             value={this.state.inputBuscar}
-            onChange={e => this.setState({inputBuscar: e.target.value})}
-            />
+            onChange={(e) => this.setState({ inputBuscar: e.target.value })}
+          />
 
           <StyledFormControl variant="filled">
-            <InputLabel 
-                id="demo-simple-select-filled-label"
-            >
+            <InputLabel id="demo-simple-select-filled-label">
               Ordenar
             </InputLabel>
             <Select
               labelId="demo-simple-select-filled-label"
               id="ordenar"
               value={this.state.inputOrdenar}
-              onChange={e => this.setState({ inputOrdenar: e.target.value })}
+              onChange={(e) => this.setState({ inputOrdenar: e.target.value })}
             >
               <MenuItem value="">
                 <em>None</em>
               </MenuItem>
-              <MenuItem value='precoCrescente'>Preço - Crescente</MenuItem>
-              <MenuItem value='precoDecrescente'>Preço - Decrescente</MenuItem>
-              <MenuItem value='prazo'>Prazo</MenuItem>
-              <MenuItem value='titulo'>Título</MenuItem>
+              <MenuItem value="precoCrescente">Preço - Crescente</MenuItem>
+              <MenuItem value="precoDecrescente">Preço - Decrescente</MenuItem>
+              <MenuItem value="prazo">Prazo</MenuItem>
+              <MenuItem value="titulo">Título</MenuItem>
             </Select>
           </StyledFormControl>
-          <StyledInput 
-            placeholder="Valor máximo" 
-            onChange={e => {
-                if(e.target.value)
-                    this.setState({ inputValorMaximo: Number(e.target.value) })
-                else
-                    this.setState({ inputValorMaximo: Infinity })
+          <StyledInput
+            placeholder="Valor máximo"
+            onChange={(e) => {
+              if (e.target.value)
+                this.setState({ inputValorMaximo: Number(e.target.value) });
+              else this.setState({ inputValorMaximo: Infinity });
             }}
-            />
-          <StyledInput 
-            placeholder="Valor mínimo" 
-            onChange={e => {
-                if(e.target.value)
-                    this.setState({ inputValorMinimo: Number(e.target.value) })
-                else
-                    this.setState({ inputValorMinimo: -Infinity })
+          />
+          <StyledInput
+            placeholder="Valor mínimo"
+            onChange={(e) => {
+              if (e.target.value)
+                this.setState({ inputValorMinimo: Number(e.target.value) });
+              else this.setState({ inputValorMinimo: -Infinity });
             }}
-            />
+          />
         </StyledContainerBusca>
 
         <StyledContainerCards>{listaFinalServicos}</StyledContainerCards>
