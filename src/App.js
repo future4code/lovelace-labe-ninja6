228256cee193ebb75452f649530b7f88d42d91f3
Gustavo Guimaraes <Gustavo.carvg@gmail.com>
2021-08-05 15:20:16 -0300

    A página inicial e a 'quero ser um ninja' já estão responsívas

--- src/App.js
+++ src/App.js
@@ -1,73 +1,87 @@
-import React from 'react'
-import * as HomeStyles from "./App.styles"
+import React from "react";
+import * as HomeStyles from "./App.styles";
 import { ThemeProvider } from "@material-ui/styles";
 import { theme } from "./theme";
-
+import Grid from "@material-ui/core/Grid";
 //Componentes
-import CardServico from './components/CardServico'
-import Carrinho from './components/ComponentCarrinho/Carrinho'
-import Contratar from './components/Contratar'
-import Home from './components/ComponentHome/Home'
-import QueroSerUmNinja from './components/QueroSerUmNinja'
+import CardServico from "./components/CardServico";
+import Carrinho from "./components/ComponentCarrinho/Carrinho";
+import Contratar from "./components/Contratar";
+import Home from "./components/ComponentHome/Home";
+import QueroSerUmNinja from "./components/QueroSerUmNinja";
 
 //Images
 import logoHeader from "./img/logo_header.png";
 
-
 class App extends React.Component {
-    state = {
-        paginaAtual: 'home'
-    }
+  state = {
+    paginaAtual: "home",
+  };
 
-    paginaSelecionada = () => {
-        switch (this.state.paginaAtual) {
-            case 'home':
-                return <Home
-                    onClickQueroSerNinja={() => this.setState({ paginaAtual: "queroserumninja" })}
-                    onClickContratarNinjas={() => this.setState({ paginaAtual: "contratar" })}
-                />
-
-            case 'queroserumninja':
-                return <QueroSerUmNinja
-                    BotaoVoltar={() => this.setState({ paginaAtual: "home" })}
-                />
-
-            case 'carrinho':
-                return <Carrinho
-                    BotaoVoltar={() => this.setState({ paginaAtual: "contratar" })}
-                />
-
-            case 'contratar':
-                return <Contratar
-                    BotaoVoltar={() => this.setState({ paginaAtual: "home" })}
-                    VerDetalhes={() => this.setState({ paginaAtual: "verdetalhes" })}
-                    AddCarrinho={() => this.setState({ paginaAtual: "carrinho" })}
-                />
-
-            case 'verdetalhes':
-                return <CardServico
-                    BotaoVoltar={() => this.setState({ paginaAtual: "contratar" })}
-                />
-
-            default:
-                return <p>Erro</p>
-        }
-    }
-
-
-    render() {
+  paginaSelecionada = () => {
+    switch (this.state.paginaAtual) {
+      case "home":
         return (
-            <ThemeProvider theme={theme}>
-                <HomeStyles.Header>
-                    <HomeStyles.LogoPeq onClick={() => this.setState({ paginaAtual: "home" })} src={logoHeader} alt="Logo da LabeNinjas" />
-                </HomeStyles.Header>
+          <Home
+            onClickQueroSerNinja={() =>
+              this.setState({ paginaAtual: "queroserumninja" })
+            }
+            onClickContratarNinjas={() =>
+              this.setState({ paginaAtual: "contratar" })
+            }
+          />
+        );
 
+      case "queroserumninja":
+        return (
+          <QueroSerUmNinja
+            BotaoVoltar={() => this.setState({ paginaAtual: "home" })}
+          />
+        );
 
-                {this.paginaSelecionada("home")}
+      case "carrinho":
+        return (
+          <Carrinho
+            BotaoVoltar={() => this.setState({ paginaAtual: "contratar" })}
+          />
+        );
 
-            </ThemeProvider>
-        )
+      case "contratar":
+        return (
+          <Contratar
+            BotaoVoltar={() => this.setState({ paginaAtual: "home" })}
+            VerDetalhes={() => this.setState({ paginaAtual: "verdetalhes" })}
+            AddCarrinho={() => this.setState({ paginaAtual: "carrinho" })}
+          />
+        );
+
+      case "verdetalhes":
+        return (
+          <CardServico
+            BotaoVoltar={() => this.setState({ paginaAtual: "contratar" })}
+          />
+        );
+
+      default:
+        return <p>Erro</p>;
     }
+  };
+
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <HomeStyles.Header>
+          <HomeStyles.LogoPeq
+            onClick={() => this.setState({ paginaAtual: "home" })}
+            src={logoHeader}
+            alt="Logo da LabeNinjas"
+          />
+        </HomeStyles.Header>
+
+        {this.paginaSelecionada("home")}
+      </ThemeProvider>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/ComponentHome/Home.styles.js
+++ src/components/ComponentHome/Home.styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const HomeStyles = {
-ContainerHome: styled.div`
+    ContainerHome: styled.div `
     display: flex;
     flex-direction: column;
     box-sizing: border-box;
@@ -10,19 +10,19 @@ ContainerHome: styled.div`
     background-color: #25274D;    
 `,
 
-ContainerLogoGrande : styled.div`
+    ContainerLogoGrande: styled.div `
     display: flex;
     justify-content: center;
     margin-top: 40px;
     margin-bottom: 30px;
 `,
 
- LogoGrande : styled.img`
+    LogoGrande: styled.img `
     height: 300px;
     width: 300px;        
 `,
 
- ContainerBotoes : styled.div`
+    ContainerBotoes: styled.div `
     display: flex;
     justify-content: space-evenly;
     margin: 10px 400px;
@@ -31,7 +31,16 @@ ContainerLogoGrande : styled.div`
         background-color: #29648A;
         color: white;
     }
+    @media (min-width: 0px) {
+        margin: 4px;
+        Button{
+            margin: 4px;
+        }
+      }
+    @media (min-width: 768px) {
+        margin: 7px
+      }
+
+
 `
 }
-
-
