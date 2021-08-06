8be0a719a92e54713ef23adf36a38b61e8284340
tiagoharumitsukodama <tiago.kodama@gmail.com>
2021-08-06 14:27:35 -0300

    cardServiços

--- src/components/CardServico.js
+++ src/components/CardServico.js
@@ -74,14 +74,12 @@ export default class CardServico extends React.Component {
             return (
               <Servico key={job.id}>
                 <h1>{job.title}</h1>
-
                 <div>
                   <p>{job.description}</p>
-
                   <p>
                     Prazo:
                     {" " +
-                      date.getDate() +
+                      date.getDay() +
                       "/" +
                       date.getMonth() +
                       "/" +
@@ -95,11 +93,10 @@ export default class CardServico extends React.Component {
                         })}
                     </strong>
                   </p>
-
                   <p>
                     Aceita:{" "}
                     {job.paymentMethods.map((payment) => {
-                      return <span>{payment}</span>;
+                      return <span key={payment}>{payment}</span>;
                     })}
                   </p>
                 </div>
